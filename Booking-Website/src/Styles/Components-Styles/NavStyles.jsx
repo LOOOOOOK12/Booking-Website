6e80393a60365b81aed8adade675600bd9be3cd3
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-25 18:18:15 +0800

    update

--- Booking-Website/src/Styles/Components-Styles/NavStyles.jsx
+++ Booking-Website/src/Styles/Components-Styles/NavStyles.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 
 const NavStyles = styled.div`
-            height: 28px;
+            height: 40px;
             top: 0;
             z-index: 15;
             width: 90%;
@@ -13,7 +13,7 @@ const NavStyles = styled.div`
             text-align: center;
             padding: 1rem 5%;
             transition: 0.5s;
-            background: red;
+            background: #F0EBD8;
 
             ul{
                 color: black;
@@ -23,7 +23,7 @@ const NavStyles = styled.div`
 
             .Links{
                 color: black;
-                margin: 0 3rem;
+                margin-right:3rem;
                 text-decoration: none;
             }
 `
