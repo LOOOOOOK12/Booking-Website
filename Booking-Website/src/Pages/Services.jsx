de4f2872ccdf9e6492e783817279efca209985b3
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-25 18:18:12 +0800

    update

---
+++ Booking-Website/src/Pages/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+import Footer from '../Components/Footer'
+
+
+function Services() {
+    return (
+        <div>
+            <NavBar/>
+            <h1>Services</h1>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Services
