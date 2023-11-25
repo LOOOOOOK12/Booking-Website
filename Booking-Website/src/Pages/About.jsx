463652511d236d191662cb78c9085a6dcc75ce9d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-25 18:18:07 +0800

    update

---
+++ Booking-Website/src/Pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+import Footer from '../Components/Footer'
+
+function About() {
+    return (
+        <div>
+            <NavBar/>
+                <h1>About</h1>
+            <Footer/>
+        </div>
+    )
+}
+
+export default About
