6cc02e11305f0b1f48741048a8f7af07b8410684
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-25 17:48:14 +0800

    update

--- Booking-Website/src/Pages/Contacts.jsx
+++ Booking-Website/src/Pages/Contacts.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
 import Footer from '../Components/Footer'
-import ContactsStyles from '../Styles/Pages-Styles/contactsStyles'
+import ContactsStyles from '../Styles/Pages-Styles/ContactsStyles'
 
 function Contacts() {
     return (
         <ContactsStyles>
             <NavBar/>
+            <div className="Container">
                 <h1>Contacts Page</h1>
+            </div>
             <Footer/>
         </ContactsStyles>
     )
