261ce1cce80f5de69bd96115f36e6a0e9591f61d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-25 17:48:10 +0800

    update

--- Booking-Website/src/Pages/Book.jsx
+++ Booking-Website/src/Pages/Book.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
 import Footer from '../Components/Footer'
-
+import BookStyles from '../Styles/Pages-Styles/BookStyles'
 
 function Book() {
     return (
-        <>
+        <BookStyles>
             <NavBar/>
-                <h1>Book Page</h1> 
+            <div className="Container">
+                <h1>Book Page</h1>
+            </div>
             <Footer/>    
-        </>
+        </BookStyles>
     )
 }
 
