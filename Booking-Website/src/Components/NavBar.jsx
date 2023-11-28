5d6344a0f84181cde0b69eec5958d7b875265672
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-28 20:46:41 +0800

    update

--- Booking-Website/src/Components/NavBar.jsx
+++ Booking-Website/src/Components/NavBar.jsx
@@ -5,14 +5,13 @@ import {Link} from 'react-router-dom'
 function NavBar() {
     return (
         <NavStyles>
-            <h1>Nav</h1>
+            <h1>LOGO</h1>
             <ul className='Link'>
                 <Link to="/Home" className='Links'>Home</Link>
                 <Link to="/About" className='Links'>About Us</Link>
                 <Link to="/Blogs" className='Links'>Blogs</Link>
                 <Link to="/Services" className='Links'>Services</Link>
-                <Link to="/Contacts" className='Links'>Contacts</Link>
-                
+                <Link to="/Contacts" className='Links'>Contacts</Link>  
             </ul>
             <Link to="/Book" className='Links'><button className='Book'>Book Now</button></Link>
         </NavStyles>
