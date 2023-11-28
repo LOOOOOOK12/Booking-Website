c2392c738a3fd03b37972d0c5fadf6d0cb1cab85
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-11-28 20:47:22 +0800

    update

---
+++ Booking-Website/src/Styles/Pages-Styles/BlogsStyles.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const BlogsStyles = styled.div`
+    .Container{
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    h1{
+        color: black;
+    }
+
+`
+export default BlogsStyles
