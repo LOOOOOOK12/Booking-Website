5ae3497a809b430a15330e1f9adc129791dc6901
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-02 16:13:02 +0800

    update

---
+++ Booking-Website/src/Constants/Available-Container/Available-Contents.js
@@ -0,0 +1,23 @@
+export const availableContents = [
+
+        {
+            image: random,
+            title: "Location",
+            locaitons: ["Kyoto", "Akihabara", "Shibuya"]
+        },
+        {
+            image: random,
+            title: "Person",
+            locaitons: ["Kyoto", "Akihabara", "Shibuya"]
+        },
+        {
+            image: random,
+            title: "Check-In",
+            locaitons: ["Kyoto", "Akihabara", "Shibuya"]
+        },
+        {
+            image: random,
+            title: "Check-Out",
+            locaitons: ["Kyoto", "Akihabara", "Shibuya"]
+        },
+]
