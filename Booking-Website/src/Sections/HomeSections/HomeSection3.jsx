b3cc7f0cc369a2258ea5c6a4ff409afafc2cf8f2
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-03 15:35:18 +0800

    update

---
+++ Booking-Website/src/Sections/HomeSections/HomeSection3.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeSection3Styles from '../../Styles/SectionsStyles/HomeSectionsStyles/HomeSection3Styles'
+
+function HomeSection3() {
+    return (
+        <HomeSection3Styles>HomeSection3</HomeSection3Styles>
+    )
+}
+
+export default HomeSection3
---
+++ Booking-Website/src/Styles/SectionsStyles/HomeSectionsStyles/HomeSection2Styles.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+const HomeSection2Styles = styled.div`
+    
+        padding: 5rem 6rem;
+    
+    h2{
+        margin: 0;
+        font-size: 50px;
+        color: #0D1321;
+    }
+    
+    .Text2{
+        width: 30rem;
+        margin-bottom: 3rem;
+    }
+
+    .Text2 p{
+        color: #7B7B7B;
+        font-size: 25px;
+        font-weight: 350;
+    }
+
+
+    .hotel-Front-Container{
+        display: flex;
+        gap: 3rem;
+    }
+
+    .Hotels{
+        display: flex;
+    }
+
+`
+
+export default HomeSection2Styles
