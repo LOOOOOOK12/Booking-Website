a14db01564f80666a329af1172a5953205384413
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-04 18:19:31 +0800

    update

--- Booking-Website/src/Pages/Home.jsx
+++ Booking-Website/src/Pages/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
 import Footer from '../Components/Footer'
+import Availability from '../Components/Availability'
 import HomeStyles from '../Styles/Pages-Styles/homeStyles'
 import HomeSection1 from '../Sections/HomeSections/HomeSection1'
 import HomeSection2 from '../Sections/HomeSections/HomeSection2'
-import Availability from '../Components/Availability'
-import HotelsContainer from '../Components/HotelsContainer'
-import { Hotels } from '../Constants/Hotels/Hotels'
+import HomeSection3 from '../Sections/HomeSections/HomeSection3'
 
 function Home() {
     return (
@@ -23,7 +22,7 @@ function Home() {
                 <Availability/>
                 <HomeSection1/>
                 <HomeSection2/>
-                <section></section>
+                <HomeSection3/>
             <Footer/>
         </HomeStyles>
     )
