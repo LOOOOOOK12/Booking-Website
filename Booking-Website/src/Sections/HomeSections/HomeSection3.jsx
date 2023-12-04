cb887a5baf3023fda4d0eeab608cc18c93f654eb
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-04 21:41:01 +0800

    update

--- Booking-Website/src/Sections/HomeSections/HomeSection3.jsx
+++ Booking-Website/src/Sections/HomeSections/HomeSection3.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import HomeSection3Styles from '../../Styles/SectionsStyles/HomeSectionsStyles/HomeSection3Styles'
+import ReviewsContainer from '../../Components/ReviewsContainer'
+import { Reviews } from '../../Constants/Reviews/Review'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/autoplay';
 
 function HomeSection3() {
     return (
         <HomeSection3Styles>
-            
+            <Swiper 
+            modules={[Autoplay]}
+            // autoplay = {{
+            // disableOnInteraction:false,
+            // delay: 5000
+            // }}
+            loop
+            id='Section3-Container'>
+
+            {Reviews.map((comments, idx)=>(
+                    <SwiperSlide key={idx}>
+                        <ReviewsContainer
+                            comment={comments.comment}
+                            profilePic={comments.profilePic}
+                            customerName={comments.customerName}
+                            role={comments.role}
+                            spot={comments.spot}
+                            alt={comments.alt}
+                            style={comments.style}
+                        />  
+                    </SwiperSlide>     
+                ))}
+            </Swiper>
         </HomeSection3Styles>
     )
 }
