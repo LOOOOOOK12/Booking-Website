8311012bb4b2c644f2aad508a781bcab63e30694
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-04 21:41:14 +0800

    update

--- Booking-Website/src/Styles/SectionsStyles/HomeSectionsStyles/HomeSection3Styles.jsx
+++ Booking-Website/src/Styles/SectionsStyles/HomeSectionsStyles/HomeSection3Styles.jsx
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 
 const HomeSection3Styles = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    #Container{
+        height: 100%;
+        width: 100%;
+    }
+
+    #Section3-Container{
+        height: 100%;
+        width: 100%;
+    }
+
 
 
 `
