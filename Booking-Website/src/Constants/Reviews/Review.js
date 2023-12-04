0917fffbe3c1a5162ef40a574f3bb69779c13bf5
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-04 21:40:56 +0800

    update

---
+++ Booking-Website/src/Constants/Reviews/Review.js
@@ -0,0 +1,33 @@
+import location from '../../assets/pexels-colon-freld-2373201.jpg'
+import kita from '../../assets/kita.jpg'
+import styles from '../../Styles/Containers-Styles/reviewsContainerStyles'
+
+export const Reviews = [
+    {
+        spot: location,
+        alt:"alt",
+        comment:`"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"`,
+        profilePic:kita,
+        customerName:"Loook",
+        role: "Lobot",
+        style: styles
+    },
+    {
+        spot: location,
+        alt:"alt",
+        comment:`"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"`,
+        profilePic:kita,
+        customerName:"Kita",
+        role: "traveler",
+        style: styles
+    },
+    {
+        spot: location,
+        alt:"alt",
+        comment:`"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"`,
+        profilePic:kita,
+        customerName:"Hitori",
+        role: "traveler",
+        style: styles
+    }
+]
