8d6213f577870244da348a1486fb6c3cfe3f6ccc
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-10 15:16:10 +0800

    update

---
+++ Booking-Website/src/Components/Pic.jsx
@@ -0,0 +1,10 @@
+function Pic (props){
+    return(
+        <div className="w-full h-80 relative flex items-center justify-center">
+            <img src={props.bg} alt="" className="absolute brightness-50 h-full w-full object-cover -z-10"/>
+            <h1 className=" font-semibold text-5xl text-Puti">{props.header}</h1>            
+        </div>
+    )
+}
+
+export default Pic
