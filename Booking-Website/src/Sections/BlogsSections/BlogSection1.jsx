9cfde9374ca41f45066dd291b7dbf60b8c6ed127
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-11 18:30:34 +0800

    update

---
+++ Booking-Website/src/Sections/BlogsSections/BlogSection1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function BlogSection1() {
+    return (
+        <div className=' px-16 py-24 flex flex-col items-center justify-center gap-12'>
+            <div className='w-3/5 text-center font-light text-3xl'>
+                <h1 className='mt-20 text-Rich-Black'>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore”
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default BlogSection1
