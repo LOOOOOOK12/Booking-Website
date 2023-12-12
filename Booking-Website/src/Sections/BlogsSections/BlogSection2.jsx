9cf8540b00a33ae60b1b0b200043486d3670e00d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-12 21:28:35 +0800

    update

--- Booking-Website/src/Sections/BlogsSections/BlogSection2.jsx
+++ Booking-Website/src/Sections/BlogsSections/BlogSection2.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import BlogsContainer from '../../Components/BlogsContainer';
+import { BlogsSwiper } from '../../Constants/Blogs/BlogsSwiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 function BlogSection2() {
     return (
-        <div>BlogSection2</div>
+        <div className='flex items-center justify-center py-20 px-24'>
+            <Swiper  
+            slidesPerView={3}
+            loop  
+            className='h-full w-full'>
+                {BlogsSwiper.map((blog,idx)=>(
+                    <SwiperSlide key={idx}>
+                        <BlogsContainer
+                            image= {blog.image}
+                            alt={blog.alt}
+                            blogs={blog.blogs}
+                        />
+                    </SwiperSlide>
+                ))}
+                
+            </Swiper>
+        </div>
     )
 }
 
