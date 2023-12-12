38d2ed431949f275ba33efa00e18acaa10d1add8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-12 21:28:26 +0800

    update

--- Booking-Website/src/Sections/BlogsSections/BlogSection1.jsx
+++ Booking-Website/src/Sections/BlogsSections/BlogSection1.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Shrine from '../../assets/Blogs/Shrine.jpg'
 
 function BlogSection1() {
     return (
-        <div className=' px-16 py-24 flex flex-col items-center justify-center gap-12'>
-            <div className='w-3/5 text-center font-light text-3xl'>
-                <h1 className='mt-20 text-Rich-Black'>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore”
-                </h1>
+        <div className=' pl-16 py-28 flex flex-row items-center justify-center gap-12'>
+            <div className='w-3/5 text-left font-light text-3xl'>
+                <h1 className='mb-4 text-[50px] font-semibold text-Rich-Black'>Nippon Sugoi</h1>
+                <hr className='mb-4 h-0.5 bg-Rich-Black' />
+                <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”</p>        
             </div>
+            <img src={Shrine} alt="Shrine" className='w-[37rem] h-[45rem] rounded-md'/>
         </div>
     )
 }
