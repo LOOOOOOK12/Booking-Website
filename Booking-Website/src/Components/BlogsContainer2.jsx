c4afbbd8357dc633d95cad1734cc24b3c3c95415
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-18 11:34:28 +0800

    update

--- Booking-Website/src/Components/BlogsContainer2.jsx
+++ Booking-Website/src/Components/BlogsContainer2.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 
 function BlogsContainer2({image, alt, blogs}) {
     return (
-        <div className='flex flex-col w-[30%] h-full'> 
-            <div className='flex gap-6 w-full h-full'>
-                <img src={image} alt={alt} className='h-[20rem] w-[30rem] rounded-md' />
+            <div className='flex mb-6 gap-6 w-full h-full'>
+                <img src={image} alt={alt} className='h-[10rem] w-[20rem] rounded-md' />
                 <div className='w-full'>
                     <p>{blogs}</p>
                 </div>
             </div>
-        </div>
     )
 }
 
