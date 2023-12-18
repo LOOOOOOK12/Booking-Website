a284390ea214eb32383cfc496f61767cb6d3ec69
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-18 16:46:19 +0800

    update

--- Booking-Website/src/Sections/AboutSections/AboutSection2.jsx
+++ Booking-Website/src/Sections/AboutSections/AboutSection2.jsx
@@ -7,22 +7,22 @@ function AboutSection2() {
             <div className='w-2/4'>
                 <div>
                     <h1 className='text-5xl font-semibold mb-4'>Our Mission</h1>
-                    <hr className='mb-4 h-0.5 bg-Rich-Black'/>
-                    <p className='text-3xl text-Rich-Black'>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    <hr className='mb-4 h-0.5 w-2/3 bg-Rich-Black'/>
+                    <p className='text-2xl text-Grey'>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                         sed do eiusmod tempor incididunt ut labore”
                     </p>
                     <div className='flex mt-6'>
                         <div className='flex flex-col mr-12'>
                             <h1 className='text-3xl font-semibold'>69%</h1>
-                            <p className='mt-1 text-Rich-Black'>"Lorem Ipsum"</p>
+                            <p className='mt-1 text-Grey'>"Lorem Ipsum"</p>
                         </div>
                         <div className='flex flex-col mr-12'>
                             <h1 className='text-3xl font-semibold'>80M+</h1>
-                            <p className='mt-1 text-Rich-Black'>"Lorem Ipsum"</p>
+                            <p className='mt-1 text-Grey'>"Lorem Ipsum"</p>
                         </div>
                         <div className='flex flex-col mr-12'>
                             <h1 className='text-3xl font-semibold'>10K</h1>
-                            <p className='mt-1 text-Rich-Black'>"Lorem Ipsum"</p>
+                            <p className='mt-1 text-Grey'>"Lorem Ipsum"</p>
                         </div>
                     </div>
                 </div>
