dfe86526aed55d25fda4474b3ef8510f47d68165
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-18 11:34:36 +0800

    update

--- Booking-Website/src/Sections/BlogsSections/BlogsSection3.jsx
+++ Booking-Website/src/Sections/BlogsSections/BlogsSection3.jsx
@@ -1,35 +1,27 @@
 import React from 'react'
 import BlogsContainer2 from '../../Components/BlogsContainer2';
-import { BlogsSwiper } from '../../Constants/Blogs/BlogsSwiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import { BlogsCollections } from '../../Constants/Blogs/BlogsCollection';
 import BlogPic1 from '../../assets/Wide/Shinto.jpg'
 
 function BlogsSection3() {
     return (
-        <div className='flex py-20 px-24'>
+        <div className='flex items-center gap-6 py-20 px-24'>
             <div className='flex flex-col'>
-                <img src={BlogPic1} alt="" className='h-[30rem] w-[35rem]'/>
+                <img src={BlogPic1} alt="" className='h-[30rem] w-[35rem] rounded-md'/>
+                <h1 className='mb-4 text-[50px] font-semibold text-Rich-Black' >Lorem Ipsum</h1>
+                <hr className='mb-4 w-2/4 h-0.5 bg-Rich-Black' />
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                     Deleniti non quibusdam ipsam,
                 </p>
             </div>
-            <div className='flex items-center w-[30rem] h-full'>
-                <Swiper  
-                slidesPerView={2}
-                loop  
-                className='h-full w-full'>
-                    {BlogsSwiper.map((blog,idx)=>(
-                        <SwiperSlide key={idx}>
-                            <BlogsContainer2
-                                image= {blog.image}
-                                alt={blog.alt}
-                                blogs={blog.blogs}
-                            />
-                        </SwiperSlide>
-                    ))}
-                    
-                </Swiper>
+            <div className='flex flex-col items-center justify-center w-[30rem] h-full'>
+                {BlogsCollections.map((blog)=>(   
+                    <BlogsContainer2
+                        image= {blog.image}
+                        alt={blog.alt}
+                        blogs={blog.blogs}
+                    />
+                ))}
             </div>
         </div>
     )
