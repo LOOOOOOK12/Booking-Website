347aa695ad7f5b1a96cc3f2e6c33cd78b9d0c0b8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-18 16:46:25 +0800

    update

--- Booking-Website/src/Sections/AboutSections/AboutSection3.jsx
+++ Booking-Website/src/Sections/AboutSections/AboutSection3.jsx
@@ -6,8 +6,8 @@ function AboutSection3() {
         <div className='px-16 py-20 flex flex-row-reverse items-center justify-between gap-20'>
             <div className='flex flex-col w-1/2'>
                 <h1 className='text-5xl font-semibold mb-4'>Our vision</h1>
-                <hr className='mb-4 h-0.5 bg-Rich-Black'/>
-                <p className='text-3xl font-light'>“Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                <hr className='mb-4 h-0.5 w-2/3 bg-Rich-Black'/>
+                <p className='text-3xl text-Grey'>“Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                     sed do eiusmod tempor incididunt ut labore”s
                 </p>
             </div>
