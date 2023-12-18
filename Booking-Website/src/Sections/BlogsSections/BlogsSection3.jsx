49fab3506bd99821020f23a2665e88b3811b3927
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-18 16:46:40 +0800

    update

--- Booking-Website/src/Sections/BlogsSections/BlogsSection3.jsx
+++ Booking-Website/src/Sections/BlogsSections/BlogsSection3.jsx
@@ -5,12 +5,12 @@ import BlogPic1 from '../../assets/Wide/Shinto.jpg'
 
 function BlogsSection3() {
     return (
-        <div className='flex items-center gap-6 py-20 px-24'>
+        <div className='flex gap-6 py-20 px-24'>
             <div className='flex flex-col'>
                 <img src={BlogPic1} alt="" className='h-[30rem] w-[35rem] rounded-md'/>
                 <h1 className='mb-4 text-[50px] font-semibold text-Rich-Black' >Lorem Ipsum</h1>
                 <hr className='mb-4 w-2/4 h-0.5 bg-Rich-Black' />
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                <p className='text-Grey '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                     Deleniti non quibusdam ipsam,
                 </p>
             </div>
@@ -19,6 +19,7 @@ function BlogsSection3() {
                     <BlogsContainer2
                         image= {blog.image}
                         alt={blog.alt}
+                        header={blog.header}
                         blogs={blog.blogs}
                     />
                 ))}
