a9cca4b31e795a9a4153f360d079b680a86e2db0
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-18 16:45:50 +0800

    update

--- Booking-Website/src/Components/BlogsContainer.jsx
+++ Booking-Website/src/Components/BlogsContainer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function BlogsContainer({image, alt, blogs}) {
+function BlogsContainer({image, alt, header, blogs}) {
     return (
         <div className='flex flex-row items-center justify-center w-[18rem] h-[30rem]'> 
             <div className='flex flex-col gap-6 w-full h-full'>
                 <img src={image} alt={alt} className='h-full w-full rounded-md' />
                 <div className='w-full'>
-                    <p>{blogs}</p>
+                    <h1 className='text-Rich-Black'>{header}</h1>
+                    <p className='text-Grey'>{blogs}</p>
                 </div>
             </div>
         </div>
