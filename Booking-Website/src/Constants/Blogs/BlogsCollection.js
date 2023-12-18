63a456cc1accebf9c50612001355e744c91b8429
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-18 11:34:31 +0800

    update

---
+++ Booking-Website/src/Constants/Blogs/BlogsCollection.js
@@ -0,0 +1,24 @@
+import Pics from '../../assets/Blogs/City.jpg'
+
+export const BlogsCollections = [
+    {
+        image: Pics,
+        alt: "Gate",
+        blogs: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" 
+    },
+    {
+        image: Pics,
+        alt: "Gate",
+        blogs: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" 
+    },
+    {
+        image: Pics,
+        alt: "Gate",
+        blogs: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" 
+    },
+    {
+        image: Pics,
+        alt: "Gate",
+        blogs: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore" 
+    }
+]
