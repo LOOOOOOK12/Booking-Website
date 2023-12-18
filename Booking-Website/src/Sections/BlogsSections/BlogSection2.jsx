e5c80ac1b2a79e3b5fa344bb03b12af216943ea0
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-18 11:34:34 +0800

    update

--- Booking-Website/src/Sections/BlogsSections/BlogSection2.jsx
+++ Booking-Website/src/Sections/BlogsSections/BlogSection2.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import BlogsContainer from '../../Components/BlogsContainer';
 import { BlogsSwiper } from '../../Constants/Blogs/BlogsSwiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper/modules';
+import 'swiper/css/free-mode';
 import 'swiper/css';
 
 function BlogSection2() {
     return (
         <div className='flex items-center justify-center py-20 px-24'>
             <Swiper  
+            modules={[FreeMode]}
             slidesPerView={3}
-            loop  
+            freeMode ={{
+                enabled: true,
+                momentum: true
+            }}
+            loop
             className='h-full w-full'>
                 {BlogsSwiper.map((blog,idx)=>(
                     <SwiperSlide key={idx}>
