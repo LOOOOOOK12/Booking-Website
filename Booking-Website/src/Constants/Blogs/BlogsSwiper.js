fa3ad0e27e7e307d7eab199c7d44eb54c77fdafd
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-18 16:46:04 +0800

    update

--- Booking-Website/src/Constants/Blogs/BlogsSwiper.js
+++ Booking-Website/src/Constants/Blogs/BlogsSwiper.js
@@ -5,31 +5,37 @@ export const BlogsSwiper = [
         {
             image: Pics,
             alt: "Gate",
+            header: "Lobot Ipsum",
             blogs: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"  
         },
         {
             image: bocchi1,
             alt: "Gate",
+            header: "Lorem Ipsum",
             blogs: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”"  
         },
         {
             image: Pics,
             alt: "Gate",
+            header: "Lorem Ipsum",
             blogs: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”"  
         },
         {
             image: Pics,
             alt: "Gate",
+            header: "Lorem Ipsum",
             blogs: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”"  
         },
         {
             image: bocchi1,
             alt: "Gate",
+            header: "Lorem Ipsum",
             blogs: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”"  
         },
         {
             image: Pics,
             alt: "Gate",
+            header: "Lorem Ipsum",
             blogs: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”"  
         },
 ]
