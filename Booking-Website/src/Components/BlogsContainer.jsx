3848bad2e95815e7c80fa3c16ee18a0118cae188
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-19 18:35:54 +0800

    update

--- Booking-Website/src/Components/BlogsContainer.jsx
+++ Booking-Website/src/Components/BlogsContainer.jsx
@@ -6,7 +6,7 @@ function BlogsContainer({image, alt, header, blogs}) {
             <div className='flex flex-col gap-6 w-full h-full'>
                 <img src={image} alt={alt} className='h-full w-full rounded-md' />
                 <div className='w-full'>
-                    <h1 className='text-Rich-Black'>{header}</h1>
+                    <h1 className='text-Rich-Black font-semibold text-2xl mb-4'>{header}</h1>
                     <p className='text-Grey'>{blogs}</p>
                 </div>
             </div>
