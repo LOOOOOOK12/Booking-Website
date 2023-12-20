41a1144e27f2f110a964164503eb9926ec022345
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-20 18:44:07 +0800

    update

---
+++ Booking-Website/src/Sections/ServicesSections/ServicesSection1.jsx
@@ -0,0 +1,14 @@
+
+
+function ServicesSection1() {
+    return (
+        <div className='pl-16 py-28 flex flex-col justify-center items-center'>
+            <h1>We offer Quality Time and Experience</h1>
+            <div className='flex '>
+
+            </div>
+        </div>
+    )
+}
+
+export default ServicesSection1
