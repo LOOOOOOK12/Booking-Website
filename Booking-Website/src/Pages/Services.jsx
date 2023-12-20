97ac5ffff1aa816e86bf5f12bc007d95c605c31b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-20 18:44:03 +0800

    update

--- Booking-Website/src/Pages/Services.jsx
+++ Booking-Website/src/Pages/Services.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
 import Footer from '../Components/Footer'
+import ServicesSection1 from '../Sections/ServicesSections/ServicesSection1'
+import ServicesSection2 from '../Sections/ServicesSections/ServicesSection2'
+import Availability from '../Components/Availability'
+import ServicesBG from '../assets/ServicesPic/Bg.jpg'
 
 function Services() {
     return (
         <>
             <NavBar/>
-                <div className='h-screen flex items-center justify-center'>
-                    <div className='Container'>
-                        <h1>Services Page</h1>
-                    </div>
-                </div> 
+                <div className="h-[90vh] relative flex items-center justify-center">
+                    <img src={ServicesBG} alt="" className='absolute brightness-50 h-full w-full object-cover -z-10' />             
+                    <h1 className='text-7xl font-semibold text-Puti'><span className='text-Silver-Lake-Blue'>Services</span></h1>
+                </div>
+                <Availability/>
+                <ServicesSection1/>
+                <ServicesSection2/>
             <Footer/>
         </>
     )
