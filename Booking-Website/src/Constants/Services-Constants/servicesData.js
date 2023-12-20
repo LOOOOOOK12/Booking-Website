fe9e6d368012d2e8d1833f28ccb480ed7e2cdc04
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-20 18:43:59 +0800

    update

---
+++ Booking-Website/src/Constants/Services-Constants/servicesData.js
@@ -0,0 +1,20 @@
+export const Reviews = [
+    {
+        img:"image",
+        alt: "image ki",
+        head:"lorem Ipsum",
+        paragraph:"lorem ipsum ausdnasdnhsadvj,shabduhsabdua"
+    },
+    {
+        img:"image",
+        alt: "image ki",
+        head:"lorem Ipsum",
+        paragraph:"lorem ipsum ausdnasdnhsadvj,shabduhsabdua"
+    },
+    {
+        img:"image",
+        alt: "image ki",
+        head:"lorem Ipsum",
+        paragraph:"lorem ipsum ausdnasdnhsadvj,shabduhsabdua"
+    }
+]
