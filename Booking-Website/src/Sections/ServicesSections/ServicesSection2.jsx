d13724d1023ed863ccdebbe7f98ecbd72c4be4a7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-20 18:44:12 +0800

    update

---
+++ Booking-Website/src/Sections/ServicesSections/ServicesSection2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServicesSection2() {
+    return (
+        <div className='h-screen flex justify-center items-center'>
+            <h1>Section 2 </h1>
+        </div>
+    )
+}
+
+export default ServicesSection2
