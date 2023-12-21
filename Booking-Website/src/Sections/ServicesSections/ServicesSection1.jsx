7d6a641a534b800c7fb8c4e1aca8e11d6f2f8120
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-21 19:21:28 +0800

    update

--- Booking-Website/src/Sections/ServicesSections/ServicesSection1.jsx
+++ Booking-Website/src/Sections/ServicesSections/ServicesSection1.jsx
@@ -1,11 +1,19 @@
-
+import ServicesContainer from "../../Components/ServicesContainer"
+import {servicesData} from "../../Constants/Services-Constants/servicesData"
 
 function ServicesSection1() {
     return (
-        <div className='pl-16 py-28 flex flex-col justify-center items-center'>
-            <h1>We offer Quality Time and Experience</h1>
-            <div className='flex '>
-
+        <div className='px-16 py-28 flex flex-col items-center justify-center gap-12'>
+            <h1 className="text-5xl mt-20 text-Rich-Black">We offer Quality Time and Experience</h1>
+            <div className='flex gap-12 w-full'>
+                {servicesData.map((services)=>(
+                        <ServicesContainer
+                        img = {services.img}
+                        alt = {services.alt}
+                        head ={services.head}
+                        paragraph = {services.paragraph}
+                    />   
+                ))}
             </div>
         </div>
     )
