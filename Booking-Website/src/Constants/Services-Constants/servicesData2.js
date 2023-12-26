7e982b89b2d4c70012f7f4744b2dede2166143b7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-26 15:16:25 +0800

    update

---
+++ Booking-Website/src/Constants/Services-Constants/servicesData2.js
@@ -0,0 +1,32 @@
+export const servicesData2 = [
+    {
+        logo:image,
+        alt:"logo",
+        head: "Lorem Ipsum",
+        objective: "lorem ipsum eiausnd  aisudh b"
+    },
+    {
+        logo:image,
+        alt:"logo",
+        head: "Lorem Ipsum",
+        do: "lorem ipsum eiausnd  aisudh b"
+    },
+    {
+        logo:image,
+        alt:"logo",
+        head: "Lorem Ipsum",
+        do: "lorem ipsum eiausnd  aisudh b"
+    },
+    {
+        logo:image,
+        alt:"logo",
+        head: "Lorem Ipsum",
+        do: "lorem ipsum eiausnd  aisudh b"
+    },
+    {
+        logo:image,
+        alt:"logo",
+        head: "Lorem Ipsum",
+        do: "lorem ipsum eiausnd  aisudh b"
+    },
+]
