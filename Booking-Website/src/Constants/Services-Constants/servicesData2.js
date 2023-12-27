3966b957c52921c248f3cb3a496468c62f80b295
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-27 14:20:18 +0800

    update

--- Booking-Website/src/Constants/Services-Constants/servicesData2.js
+++ Booking-Website/src/Constants/Services-Constants/servicesData2.js
@@ -1,32 +1,39 @@
+import image from '../../assets/ServicesPic/icon.png'
+import down from '../../assets/ServicesPic/arrow-down.png'
+
 export const servicesData2 = [
     {
         logo:image,
         alt:"logo",
         head: "Lorem Ipsum",
-        objective: "lorem ipsum eiausnd  aisudh b"
+        objective: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”",
+        arrow: down
     },
     {
         logo:image,
         alt:"logo",
         head: "Lorem Ipsum",
-        do: "lorem ipsum eiausnd  aisudh b"
+        objective: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”",
+        arrow: down
     },
     {
         logo:image,
         alt:"logo",
         head: "Lorem Ipsum",
-        do: "lorem ipsum eiausnd  aisudh b"
+        objective: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”",
+        arrow: down
     },
     {
         logo:image,
         alt:"logo",
         head: "Lorem Ipsum",
-        do: "lorem ipsum eiausnd  aisudh b"
+        objective: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”",
+        arrow: down
     },
     {
         logo:image,
         alt:"logo",
-        head: "Lorem Ipsum",
-        do: "lorem ipsum eiausnd  aisudh b"
+        head: "Lorem Ipsum sss",
+        objective: "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”",
     },
 ]
