18f479a3fb911a53ea0536163330c126b83c344d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-27 14:20:28 +0800

    update

--- Booking-Website/src/Sections/ServicesSections/ServicesSection3.jsx
+++ Booking-Website/src/Sections/ServicesSections/ServicesSection3.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function ServicesSection3() {
     return (
-        <div>
-            
+        <div className='flex justify-center items-center'>
+            <h1 className='text-3xl text-Rich-Black font-bold w-1/2 text-center'>Lobot</h1>
         </div>
     )
 }
