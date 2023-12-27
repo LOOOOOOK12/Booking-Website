a64c5871e539048839efe49257c6bb5f7b048152
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-27 14:20:10 +0800

    update

--- Booking-Website/src/Components/ServicesContainer2.jsx
+++ Booking-Website/src/Components/ServicesContainer2.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import down from '../assets/ServicesPic/arrow-down.png'
 
 function ServicesContainer2({logo, alt, head, objective}) {
     return (
-        <div className='flex flex-col items-center justify-center text-center'>
-            <img src={logo} alt={alt} />
-            <h1>{head}</h1>
-            <p>{objective}</p>
+        <div className='mb-16 flex flex-col justify-center items-center'>
+            <div className='mb-6 flex flex-col items-center justify-center gap-5 text-center'>
+                <img src={logo} alt={alt} />
+                <h1 className='text-3xl font-semibold text-Rich-Black'>{head}</h1>
+                <p className='text-Grey'>{objective}</p>
+            </div>
+            <img src={down} alt="arrow" />
         </div>
     )
     }
