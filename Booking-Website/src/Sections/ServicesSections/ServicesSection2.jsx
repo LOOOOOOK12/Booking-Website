4d708e9bb63b86c8dd151c59e296edaa92d37d43
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-27 14:20:26 +0800

    update

--- Booking-Website/src/Sections/ServicesSections/ServicesSection2.jsx
+++ Booking-Website/src/Sections/ServicesSections/ServicesSection2.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import ServicesContainer2 from '../../Components/ServicesContainer2'
+import { servicesData2 } from '../../Constants/Services-Constants/servicesData2'
 
 function ServicesSection2() {
     return (
         <div className='px-16 py-28 flex flex-col items-center justify-center'>
-            <h1 className='text-3xl text-Rich-Black font-bold w-1/2 text-center'>How We Work</h1>
-            <div>
-
+            <h1 className='mb-16 text-5xl text-Rich-Black font-bold w-1/2 text-center'>How We Work</h1>
+            <div className=' flex flex-col'>
+                {servicesData2.map((s)=>(
+                    <ServicesContainer2
+                        logo = {s.logo}
+                        alt = {s.alt}
+                        head = {s.head}
+                        objective = {s.objective}
+                        arrow ={s.arrow} 
+                    />
+                ))}
             </div>
         </div>
     )
