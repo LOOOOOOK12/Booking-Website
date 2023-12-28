c56b16468a4cbeb4620415b0c762fd6d7f019e2d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-28 16:17:01 +0800

    update

--- Booking-Website/src/Sections/BlogsSections/BlogSection2.jsx
+++ Booking-Website/src/Sections/BlogsSections/BlogSection2.jsx
@@ -8,7 +8,7 @@ import 'swiper/css';
 
 function BlogSection2() {
     return (
-        <div className='flex items-center justify-center py-20 px-24'>
+        <div className='flex items-center justify-center py-20 px-16'>
             <Swiper  
             modules={[FreeMode]}
             slidesPerView={3}
@@ -16,16 +16,30 @@ function BlogSection2() {
                 enabled: true,
                 momentum: true
             }}
+            breakpoints={{
+                    425: {
+                        slidesPerView: 1,
+                    },
+                    640: {
+                        slidesPerView: 2, 
+                    },
+                    768: {
+                        slidesPerView: 3, 
+                    },
+                }}
             loop
-            className='h-full w-full'>
+            className='h-full w-full flex justify-center items-center'
+            >
                 {BlogsSwiper.map((blog,idx)=>(
                     <SwiperSlide key={idx}>
-                        <BlogsContainer
-                            image= {blog.image}
-                            alt={blog.alt}
-                            header={blog.header}
-                            blogs={blog.blogs}
-                        />
+                            <div className='mr-8'>
+                                <BlogsContainer
+                                    image= {blog.image}
+                                    alt={blog.alt}
+                                    header={blog.header}
+                                    blogs={blog.blogs}
+                                />
+                            </div>    
                     </SwiperSlide>
                 ))}
                 
--- Booking-Website/src/Sections/BlogsSections/BlogsSection3.jsx
+++ Booking-Website/src/Sections/BlogsSections/BlogsSection3.jsx
@@ -5,7 +5,7 @@ import BlogPic1 from '../../assets/Wide/Shinto.jpg'
 
 function BlogsSection3() {
     return (
-        <div className='py-20 px-24 flex flex-col md:flex md:flex-row md:gap-10'>
+        <div className='py-20 px-16 flex flex-col gap-10 md:flex md:flex-row md:gap-10'>
             <div className='flex flex-col w-2/3'>
                 <img src={BlogPic1} alt="" className='md:h-[30rem] md:w-[35rem] rounded-md'/>
                 <h1 className='mb-4 mt-4 text-5xl font-semibold text-Rich-Black' >Lorem Ipsum</h1>
