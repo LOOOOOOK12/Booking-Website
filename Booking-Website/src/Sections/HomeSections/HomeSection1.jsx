35f2e73c120a3c62e2cbc32bcad0e88f3af58795
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2023-12-31 14:38:01 +0800

    update

--- Booking-Website/src/Sections/HomeSections/HomeSection1.jsx
+++ Booking-Website/src/Sections/HomeSections/HomeSection1.jsx
@@ -3,10 +3,10 @@ import Image1 from '../../assets/Home/pexels-evgeny-tchebotarev-2187604.jpg'
 
 function HomeSection1() {
     return (
-        <div className='pt-24 px-16 md:flex md:items-center md:justify-center md:gap-20'>
-            <img src={Image1} alt="Image 1" className='h-[29rem] w-[31qrem] rounded-md' />
+        <div className='pt-24 px-16 md:flex md:items-center md:justify-center md:gap-28'>
+            <img src={Image1} alt="Image 1" className='h-[29rem] w-[30rem] rounded-md' />
             <div className='flex-col w-full'>
-                <div className='w-2/3'>
+                <div className='w-2/3 mt-6'>
                     <h2 className='mb-4 text-5xl text-Rich-Black font-semibold'>Lorem Ipsum</h2>
                     <hr className='h-0.5 bg-Rich-Black'/>
                 </div>
