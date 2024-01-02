323e7e5c6963a217270a2d75cc6528241126c77e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-02 13:54:21 +0800

    update

--- Booking-Website/src/Pages/Contacts.jsx
+++ Booking-Website/src/Pages/Contacts.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import NavBar from '../Components/NavBar'
 import Footer from '../Components/Footer'
+import Availability from '../Components/Availability'
+import ContactsBg from '../assets/Contacts/ContactsBg.jpg'
+import ContactsSection1 from '../Sections/ContactsSections/ContactsSection1'
 
 function Contacts() {
     return (
         <>
             <NavBar/>
-                <div className='h-screen flex items-center justify-center'>
-                    <div className='Container'>
-                        <h1>Contacts Page</h1>
-                    </div>
-                </div> 
+                <div className="h-[90vh] relative flex items-center justify-center">
+                    <img src={ContactsBg} alt="" className='absolute brightness-50 h-full w-full object-cover -z-10' />             
+                    <h1 className='text-7xl font-semibold text-Puti'><span className='text-Silver-Lake-Blue'>Contacts</span></h1>
+                </div>
+                <Availability/> 
+                <ContactsSection1/>
             <Footer/>
         </>
     )
