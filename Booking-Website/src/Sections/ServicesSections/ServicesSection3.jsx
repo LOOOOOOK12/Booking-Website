9d8640058f8273642947115f18f64300ad0e7ea1
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-02 13:54:39 +0800

    update

--- Booking-Website/src/Sections/ServicesSections/ServicesSection3.jsx
+++ Booking-Website/src/Sections/ServicesSections/ServicesSection3.jsx
@@ -4,8 +4,8 @@ import { servicesData3 } from "../../Constants/Services-Constants/servicesData3"
 function ServicesSection3() {
     return (
         <div className='px-16 py-28 flex flex-col justify-center items-center gap-12'>
-            <h1 className='text-5xl text-Rich-Black font-bold w-1/2 text-center'>We Offer</h1>
-            <div className='grid grid-cols-3 gap-6'>
+            <h1 className='text-5xl text-Rich-Black font-bold w-full text-center'>We Offer</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 lg:grid-cols-3 lg:gap-6'>
                 {servicesData3.map((G)=>(
                     <ServicesGridContainer
                         logos = {G.logos}
