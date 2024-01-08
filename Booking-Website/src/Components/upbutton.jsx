6893058035d6ba11e74943cadf8c80bacb23fdb9
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-08 18:35:09 +0800

    update

---
+++ Booking-Website/src/Components/upbutton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function upbutton({up}) {
+
+    const upbutton = () => {
+        
+    }
+
+    return (
+        <div className=''>
+            <button>
+
+            </button>
+        </div>
+    )
+}
+
+export default upbutton
