10c07bc199c0cf3800afa4633ffc591c65e9b78c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-08 18:35:31 +0800

    update

--- Booking-Website/src/Sections/HomeSections/HomeSection2.jsx
+++ Booking-Website/src/Sections/HomeSections/HomeSection2.jsx
@@ -5,10 +5,10 @@ import { Hotels } from '../../Constants/Hotels/Hotels'
 function HomeSection2() {
     return (
         <div className='px-16 py-20'>
-            <div className='w-[30rem] mb-12'>
-                <div className='w-2/3'>
+            <div className='w-full mb-12'>
+                <div className='w-1/2'>
                     <h2 className='mb-4 text-5xl text-Rich-Black font-semibold'>Hotel</h2>
-                    <hr className='h-0.5 bg-Rich-Black'/>
+                    <hr className='h-0.5 w-full bg-Rich-Black'/>
                 </div>
                 <p className='mt-4 text-Grey font-light text-2xl'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
