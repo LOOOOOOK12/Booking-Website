b3ea74f1824bd9e57351dd6176aeac484f480da3
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-12 18:17:39 +0800

    update

--- Booking-Website/src/Sections/ContactsSections/ContactsSection2.jsx
+++ Booking-Website/src/Sections/ContactsSections/ContactsSection2.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ContactsSection2() {
     return (
-        <div>
+        <div className='h-screen flex items-center justify-center'>
             ContactsSection2
         </div>
     )
