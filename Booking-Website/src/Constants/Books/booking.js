333465988b0b1e05e9b29b386100f91fcdafceeb
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-25 14:53:18 +0800

    update

---
+++ Booking-Website/src/Constants/Books/booking.js
@@ -0,0 +1,46 @@
+import Hallway from '../../assets/Wide/hallway.jpg'
+
+export const booking = [
+    {
+        image:Hallway,
+        alt:"alt",
+        header:"loak",
+        description:"lorem",
+        price: "2pesos"
+    },
+    {
+        image:Hallway,
+        alt:"alt",
+        header:"loak",
+        description:"lorems",
+        price: "2pesos"
+    },
+    {
+        image:Hallway,
+        alt:"alt",
+        header:"loak",
+        description:"lorem",
+        price: "2pesos"
+    },
+    {
+        image:Hallway,
+        alt:"alt",
+        header:"loak",
+        description:"lorem",
+        price: "2pesos"
+    },
+    {
+        image:Hallway,
+        alt:"alt",
+        header:"loak",
+        description:"lorems",
+        price: "2pesos"
+    },
+    {
+        image:Hallway,
+        alt:"alt",
+        header:"loak",
+        description:"lorem",
+        price: "2pesos"
+    }
+]
