5040c129f7318b32b251c9fbdbe63ef4465e269c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-29 15:17:38 +0800

    update

--- Booking-Website/src/Components/bookContainer.jsx
+++ Booking-Website/src/Components/bookContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-function bookContainer({image, alt, header, price, description}) {
+function bookContainer({image, alt, header, price, description, count}) {
+
     return (
         <div className='flex flex-col py-10 px-9 rounded-md bg-Grey-2'>
             <img src={image} alt={alt} className='rounded-md mb-5'/>
@@ -9,7 +10,12 @@ function bookContainer({image, alt, header, price, description}) {
                 <hr className='mb-4 h-0.5 w-full bg-Rich-Black'/>
                 <h2 className='text-4xl text-Rich-Black font-semibold'>{price}</h2>
                 <p className='text-xl text-Grey-3'>{description}</p>
-                <button className='text-Puti px-6 py-5 w-full bg-Rich-Black hover:bg-Prussian-Blue ease-in duration-100 font-semibold rounded-md'>Book</button>
+                <button 
+                    className='text-Puti px-6 py-5 w-full bg-Rich-Black hover:bg-Prussian-Blue ease-in duration-100 font-semibold rounded-md'
+                    onClick={count}
+                    >
+                        Book
+                    </button>
             </div>
         </div>
     )
