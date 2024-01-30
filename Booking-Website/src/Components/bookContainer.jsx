ae3a434d168a5acfddefa46d9c33c87d700ada59
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-30 16:27:18 +0800

    update

--- Booking-Website/src/Components/bookContainer.jsx
+++ Booking-Website/src/Components/bookContainer.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
-function bookContainer({image, alt, header, price, description, count}) {
+function bookContainer({image, alt, header, price, description, visitors, count}) {
 
     return (
-        <div className='flex flex-col py-10 px-9 rounded-md bg-Grey-2'>
-            <img src={image} alt={alt} className='rounded-md mb-5'/>
-            <div className='flex flex-col items-center gap-7'>
-                <h1 className='text-5xl text-Rich-Black font-semibold'>{header}</h1>
-                <hr className='mb-4 h-0.5 w-full bg-Rich-Black'/>
-                <h2 className='text-4xl text-Rich-Black font-semibold'>{price}</h2>
-                <p className='text-xl text-Grey-3'>{description}</p>
+        <div className='flex flex-col'>
+            <img src={image} alt={alt} className='rounded-3xl mb-5'/>
+            <div className='flex flex-col items-start gap-3'>
+                <h1 className='text-4xl text-Rich-Black font-semibold'>{header}</h1>
+                <h2 className='text-2xl text-Rich-Black font-semibold'>{price}</h2>
+                <div className='flex gap-5'>
+                    <p className='text-xl text-Grey-3'>{description}</p>
+                    <p className='text-xl text-Rich-Black'>{visitors}</p>
+                </div>
                 <button 
                     className='text-Puti px-6 py-5 w-full bg-Rich-Black hover:bg-Prussian-Blue ease-in duration-100 font-semibold rounded-md'
                     onClick={count}
