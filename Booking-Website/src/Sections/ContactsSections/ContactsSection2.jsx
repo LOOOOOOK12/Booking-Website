9ed08547bc540807000bad30ab10c959cae48710
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-01-30 16:41:47 +0800

    update

--- Booking-Website/src/Sections/ContactsSections/ContactsSection2.jsx
+++ Booking-Website/src/Sections/ContactsSections/ContactsSection2.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 
 function ContactsSection2() {
     return (
-        <div className='h-screen flex items-center justify-center'>
-            ContactsSection2
+        <div className='py-20 px-16 flex justify-center items-center gap-10'>
+            <div className='flex flex-col'>
+                <h1 className=' text-Rich-Black text-3xl font-semibold mb-5'>Techniccal Support</h1>
+                <div className='flex flex-col gap-3'>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                </div>
+            </div>
+            <div className='flex flex-col'>
+            <h1 className=' text-Rich-Black text-3xl font-semibold mb-5'>Mechanical Support</h1>
+                <div className='flex flex-col gap-3'>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                </div>
+            </div>
+            <div className='flex flex-col'>
+            <h1 className=' text-Rich-Black text-3xl font-semibold mb-5'>Support</h1>
+                <div className='flex flex-col gap-3'>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                    <p>lorem</p>
+                </div>
+            </div>
         </div>
     )
 }
