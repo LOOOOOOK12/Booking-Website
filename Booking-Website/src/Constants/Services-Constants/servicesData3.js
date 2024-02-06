81868b778515930185b79aefc29c2fb9e882336a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-06 17:52:46 +0800

    update

--- Booking-Website/src/Constants/Services-Constants/servicesData3.js
+++ Booking-Website/src/Constants/Services-Constants/servicesData3.js
@@ -2,26 +2,32 @@ import logo from '../../assets/ServicesPic/icon.png'
 
 export const servicesData3 = [
     {
+        header:"Lorem",
         logos: logo,
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
     },
     {
+        header:"Lorem",
         logos: logo,
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
     },
     {
+        header:"Lorem",
         logos: logo,
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
     },
     {
+        header:"Lorem",
         logos: logo,
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
     },
     {
+        header:"Lorem",
         logos: logo,
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
     },
     {
+        header:"Lorem",
         logos: logo,
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
     }  
