4fbaaf4977cc8f52ca78af98c5b466f3a7d19f62
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-08 22:47:44 +0800

    update

--- Booking-Website/src/Sections/BookSection/BookSection1.jsx
+++ Booking-Website/src/Sections/BookSection/BookSection1.jsx
@@ -12,8 +12,11 @@ function BookSection1() {
     }
 
     return (
-        <div className='py-20 px-16 flex flex-col gap-8 items-center justify-between'>
-            <h1>Total Booked: {count}</h1>
+        <div className='py-20 px-16 flex flex-col gap-8 '>
+            <div className='flex flex-col gap-8'>
+                <h1 className='text-Rich-Black text-3xl font-semibold'>Total Booked: {count}</h1>
+                <h1 className='text-Rich-Black text-3xl font-semibold'>Total available Hotels: {booking.length}</h1>
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
                 {booking.map((B) =>(
                     <BookContainer
