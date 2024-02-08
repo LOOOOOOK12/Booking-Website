63bc6bfeed6ed142cd93e8748e74f7b50d5a1420
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-08 22:47:36 +0800

    update

--- Booking-Website/src/Pages/Book.jsx
+++ Booking-Website/src/Pages/Book.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Blogsbg from '../assets/Blogs/City.jpg'
 import BookSection1 from '../Sections/BookSection/BookSection1'
 import BookSEction2 from '../Sections/BookSection/BookSEction2'
 import NavBar from '../Components/NavBar'
@@ -8,6 +9,10 @@ function Book() {
     return (
         <>
             <NavBar/>
+                <div className="h-[90vh] relative flex items-center justify-center">
+                    <img src={Blogsbg} alt="" className='absolute brightness-50 h-full w-full object-cover -z-10' />             
+                    <h1 className='text-Silver-Lake-Blue text-7xl font-semibold'>Booking</h1>
+                </div>
                 <BookSection1/>
                 <BookSEction2/>
             <Footer/>
