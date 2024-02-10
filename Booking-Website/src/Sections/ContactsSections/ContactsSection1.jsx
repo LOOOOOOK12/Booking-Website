404e88012ed5ae564772a55b23b14306af07b7b8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-10 16:08:03 +0800

    update

--- Booking-Website/src/Sections/ContactsSections/ContactsSection1.jsx
+++ Booking-Website/src/Sections/ContactsSections/ContactsSection1.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ContactsSection1() {
     return (
         <div className='py-20 px-16 flex flex-col gap-8 md:flex-row items-center justify-between'>
-            <form className='flex flex-col gap-3'>
+            <form className='flex flex-col gap-3 w-3/5'>
                 <label  className='text-Rich-Black text-xl'>Enter Name:</label>
                 <input type="text" className='p-2 bg-Puti border-Prussian-Blue border-2 rounded-md' />
                 <label className='text-Rich-Black text-xl'>Enter Email:</label>
@@ -11,7 +11,7 @@ function ContactsSection1() {
                 <label className=' text-Rich-Black text-xl'>Send a Message:</label>
                 <textarea disabled cols="30" rows="10" className='py-2 bg-Puti rounded-md border-Prussian-Blue border-2's/>
             </form>
-            <div className=''>
+            <div className='w-3/4'>
                 <h1 className='mb-5 text-Rich-Black text-5xl font-semibold'>Message Us!</h1>
                 <p className='text-Grey text-2xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae deleniti libero numquam nemo 
                     consequuntur delectus molestiae sunt impedit nisi. Hic, inventore explicabo asperiores eos odio maiores 
