a2c7ea18481b818167d3415eefd308f44cfa343f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-10 16:07:54 +0800

    update

--- Booking-Website/src/Sections/BlogsSections/BlogSection1.jsx
+++ Booking-Website/src/Sections/BlogsSections/BlogSection1.jsx
@@ -5,8 +5,8 @@ function BlogSection1() {
     return (
         <div className=' py-20 px-16 flex flex-col-reverse gap-8 md:flex md:flex-row md:items-center md:justify-center md:gap-8'>
             <div className=' w-full md:w-2/3 md:text-left'>
-                <div className='flex flex-col w-1/2'>
-                    <h2 className='w-2/3 text-5xl font-semibold mb-4 text-Rich-Black'>Nippon Sugoi</h2>
+                <div className='flex flex-col w-2/3'>
+                    <h2 className='h-full text-5xl font-semibold mb-4 text-Rich-Black'>Nippon Sugoi</h2>
                     <hr className='mb-4 h-0.5 w-full bg-Rich-Black'/>
                 </div>
                 <p className='text-2xl text-Grey'>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore”</p>        
