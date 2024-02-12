5fa738caab0c5dcca55bdc6e6ebc9be0cbe83fb8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-12 19:12:24 +0800

    update

--- Booking-Website/src/Components/Footer.jsx
+++ Booking-Website/src/Components/Footer.jsx
@@ -3,15 +3,15 @@ import {Link} from 'react-router-dom'
 
 function Footer() {
     return (
-        <div className='p-6 flex flex-col justify-evenly items-center gap-4 bg-Prussian-Blue text-Puti '>
+        <div className='p-6 flex flex-col justify-evenly items-center gap-5 bg-Prussian-Blue text-Puti '>
             <div className='w-full h-full flex justify-evenly'>
-                <div className='justify-start'>
+                <div className='justify-start flex flex-col gap-2'>
                     <h2 className='mb-4 font-semibold text-2xl'>Contacts</h2>
-                    <p>Lorem</p>
-                    <p>Lorem</p>
-                    <p>Lorem</p>
+                    <p>Webiste.com</p>
+                    <p>Website@gmail.com</p>
+                    <p>Employees</p>
                 </div>
-                <div className='justify-start'>
+                <div className='justify-start flex flex-col gap-2'>
                     <h2 className='mb-4 font-semibold text-2xl'>Menu</h2>
                     <Link to="/Home" className='Links'><p>Home</p></Link>
                     <Link to="/About" className='Links'><p>About Us</p></Link>
@@ -19,11 +19,11 @@ function Footer() {
                     <Link to="/Services" className='Links'><p>Services</p></Link>
                     <Link to="/Contacts" className='Links'><p>Contacts</p></Link> 
                 </div>
-                <div className='justify-start'>
+                <div className='justify-start flex flex-col gap-2'>
                     <h2 className='mb-4 font-semibold text-2xl'>Media</h2>
-                    <p>Lorem</p>
-                    <p>Lorem</p>
-                    <p>Lorem</p>
+                    <p>Website.twitter</p>
+                    <p>Facebook</p>
+                    <p>Maps</p>
                 </div>
             </div>
             <p>Made with ❤︎‬ by Luke Symon Alamil</p>
