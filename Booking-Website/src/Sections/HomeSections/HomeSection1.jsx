c821d7cd56c69bdde57b21fa41ab02c4bf3ec2d8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-13 15:13:03 +0800

    update

--- Booking-Website/src/Sections/HomeSections/HomeSection1.jsx
+++ Booking-Website/src/Sections/HomeSections/HomeSection1.jsx
@@ -10,10 +10,10 @@ function HomeSection1() {
                     <h2 className='mb-4 text-5xl text-Rich-Black font-semibold'>Lorem Ipsum</h2>
                     <hr className='h-0.5 bg-Rich-Black'/>
                 </div>
-                <p className='text-Grey text-2xl mt-3.5 font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                <p className='text-Grey text-2xl mt-3.5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 </p>
-                <button className='mt-6 px-11 py-6 text-Puti text-lg font-light bg-Rich-Black hover:bg-Prussian-Blue ease-in duration-100 rounded-md'>Read More</button>
+                <button className='mt-6 px-11 py-6 text-Puti text-lg font-semibold bg-Rich-Black hover:bg-Prussian-Blue ease-in duration-100 rounded-md'>Read More</button>
             </div>
         </div>
     )
