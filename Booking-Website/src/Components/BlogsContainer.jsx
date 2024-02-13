23d55937fe860046dded2eacb034c31416ada7a2
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-13 15:12:40 +0800

    update

--- Booking-Website/src/Components/BlogsContainer.jsx
+++ Booking-Website/src/Components/BlogsContainer.jsx
@@ -6,7 +6,7 @@ function BlogsContainer({image, alt, header, blogs}) {
                 <img src={image} alt={alt} className='h-[25rem] w-full rounded-md' />
                 <div className='w-full'>
                     <h1 className='text-Rich-Black font-semibold text-3xl mb-4'>{header}</h1>
-                    <p className='text-Grey'>{blogs}</p>
+                    <p className='text-Grey text-xl'>{blogs}</p>
                 </div>
             </div>
     )
