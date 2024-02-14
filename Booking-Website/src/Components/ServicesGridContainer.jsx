cfd03fbc4cea68c3a10b11c78eb6525788f2cefa
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-14 21:08:35 +0800

    update

--- Booking-Website/src/Components/ServicesGridContainer.jsx
+++ Booking-Website/src/Components/ServicesGridContainer.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function ServicesGridContainer({logos,header,desc}) {
     return (
-        <div className='flex flex-col items-center justify-center gap-6 px-6 py-8 bg-Grey-2 rounded-md'>
-            <img src={logos} alt=""  className='h-12 w-10'/>
-            <h1 className='text-3xl font-semibold text-Rich-Black'>{header}</h1>
-            <p className='text-Grey'>{desc}</p>
+        <div className='flex flex-col items-center justify-center gap-10 px-6 py-8 bg-Grey-2 rounded-md'>
+            <img src={logos} alt="logo"  className='h-14 w-14'/>
+            <div className='flex flex-col gap-5 text-left'>
+                <h1 className='text-4xl font-semibold text-Rich-Black'>{header}</h1>
+                <p className='text-Grey text-xl'>{desc}</p>
+            </div>
         </div>
     )
 }
