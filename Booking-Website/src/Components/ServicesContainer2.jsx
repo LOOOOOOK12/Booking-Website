f3889ab3c798f5b40341048c4d62654f85bcc37c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-14 21:08:31 +0800

    update

--- Booking-Website/src/Components/ServicesContainer2.jsx
+++ Booking-Website/src/Components/ServicesContainer2.jsx
@@ -5,8 +5,8 @@ function ServicesContainer2({logo, alt, head, objective, arrow}) {
         <div className='mb-16 flex flex-col justify-center items-center'>
             <div className='mb-6 flex flex-col items-center justify-center gap-5 text-center'>
                 <img src={logo} alt={alt} />
-                <h1 className='text-3xl font-semibold text-Rich-Black'>{head}</h1>
-                <p className='text-Grey'>{objective}</p>
+                <h1 className='text-4xl font-semibold text-Rich-Black'>{head}</h1>
+                <p className='text-Grey text-2xl'>{objective}</p>
             </div>
             <img src={arrow} />
         </div>
