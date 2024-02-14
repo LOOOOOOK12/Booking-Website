cce3a0374724eb4336fd56621baad2813263515e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-14 21:08:22 +0800

    update

--- Booking-Website/src/Components/NavBar.jsx
+++ Booking-Website/src/Components/NavBar.jsx
@@ -13,11 +13,11 @@ function NavBar() {
             <nav className='px-16 p-4 w-full top-0 z-10 fixed flex justify-between items-center text-center bg-Bonay'>
                 <Link to="/Home" className='text-3xl'><h1>LOGO</h1></Link>
                 <ul className='hidden sm:flex sm:flex-row'>
-                    <Link to="/Home" className={`text-Rich-Black font-semibold mr-12 ${isActive("/Home") && 'font-black'}`}>Home</Link>
-                    <Link to="/About" className={`text-Rich-Black font-semibold mr-12 ${isActive("/About") && 'font-black'}`}>About Us</Link>
-                    <Link to="/Blogs" className={`text-Rich-Black font-semibold mr-12 ${isActive("/Blogs") && 'font-black'}`}>Blogs</Link>
-                    <Link to="/Services" className={`text-Rich-Black font-semibold mr-12 ${isActive("/Services") && 'font-black'}`}>Services</Link>
-                    <Link to="/Contacts" className={`text-Rich-Black font-semibold mr-12 ${isActive("/Contacts") && 'font-black'}`}>Contacts</Link>
+                    <Link to="/Home" className={`text-Rich-Black font-medium mr-12 ${isActive("/Home") && 'font-black'}`}>Home</Link>
+                    <Link to="/About" className={`text-Rich-Black font-medium mr-12 ${isActive("/About") && 'font-black'}`}>About Us</Link>
+                    <Link to="/Blogs" className={`text-Rich-Black font-medium mr-12 ${isActive("/Blogs") && 'font-black'}`}>Blogs</Link>
+                    <Link to="/Services" className={`text-Rich-Black font-medium mr-12 ${isActive("/Services") && 'font-black'}`}>Services</Link>
+                    <Link to="/Contacts" className={`text-Rich-Black font-medium mr-12 ${isActive("/Contacts") && 'font-black'}`}>Contacts</Link>
                 </ul>
                     <Link to="/Book" className='Links'>
                         <button className='text-Puti px-6 py-3 bg-Rich-Black hover:bg-Prussian-Blue ease-in duration-100 font-semibold rounded-md max-[860px]:hidden'>Book Now</button>
