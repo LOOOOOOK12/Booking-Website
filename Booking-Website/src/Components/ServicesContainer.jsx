78d750be088b4bf6fee7c9dd42337892721cd0b1
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-14 21:08:26 +0800

    update

--- Booking-Website/src/Components/ServicesContainer.jsx
+++ Booking-Website/src/Components/ServicesContainer.jsx
@@ -4,9 +4,9 @@ function ServicesContainer({img, alt, head, paragraph}) {
     return (
         <div className='px-6 py-10 flex flex-col items-center justify-center gap-10 bg-Grey-2 rounded-md'>
             <img src={img} alt={alt} className='rounded-md h-12 w-15'/>
-            <div className=' flex flex-col gap-5 text-left'>
-                <h1 className='text-3xl font-semibold text-Rich-Black'>{head}</h1>
-                <p className='text-Grey'>{paragraph}</p>
+            <div className='flex flex-col gap-5 text-left'>
+                <h1 className='text-4xl font-semibold text-Rich-Black'>{head}</h1>
+                <p className='text-Grey text-xl'>{paragraph}</p>
             </div>
         </div>
     )
