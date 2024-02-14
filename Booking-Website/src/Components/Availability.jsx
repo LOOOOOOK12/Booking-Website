b1a9a0d00ad11e49613b811393a166902bb8f4fd
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-14 21:08:17 +0800

    update

--- Booking-Website/src/Components/Availability.jsx
+++ Booking-Website/src/Components/Availability.jsx
@@ -9,7 +9,7 @@ function Availability() {
         <div className='relative flex w-full items-center justify-center max-[955px]:hidden '>
             <div className='absolute flex items-center justify-center gap-8 py-4 px-10 rounded-lg bg-[#F0EBD8] '>
                 <div className='flex flex-col'>
-                    <div className='flex gap-2 items-center my-3'>
+                    <div className='flex gap-2 font-semibold items-center my-3'>
                         <img src={location} alt="location" className='h-6' />
                         <p>Location</p>
                     </div>
@@ -17,7 +17,7 @@ function Availability() {
                 </div> 
                 <div className='h-20 w-0.5 bg-Rich-Black'></div>
                     <div className='flex flex-col'>
-                        <div className='flex gap-2 items-center my-3'>
+                        <div className='flex gap-2 font-semibold items-center my-3'>
                             <img src={person} alt="person" className='h-6' />
                             <p>Person</p>
                         </div>
@@ -25,7 +25,7 @@ function Availability() {
                     </div>
                 <div className='h-20 w-0.5 bg-Rich-Black'></div>
                     <div className='flex flex-col'>
-                        <div className='flex gap-2 items-center my-3'>
+                        <div className='flex gap-2 font-semibold items-center my-3'>
                             <img src={calendar} alt="calendar" className='h-6'/>
                             <p>Check-In</p>
                         </div>
@@ -33,13 +33,13 @@ function Availability() {
                     </div>
                 <div className='h-20 w-0.5 bg-Rich-Black'></div>
                     <div className='flex flex-col'>
-                        <div className='flex gap-2 items-center my-3'>
+                        <div className='flex gap-2 font-semibold items-center my-3'>
                             <img src={calendar} alt="calendar" className='h-6' />
                             <p>Check-Out</p>
                         </div>
                     <p>May 25, 2023</p>
                 </div>
-                <Link to="/Book"><button className='text-lg text-Puti font-light rounded py-5 px-6 bg-Rich-Black hover:bg-Prussian-Blue ease-in     '> Check Availability</button></Link>
+                <Link to="/Book"><button className='text-lg text-Puti font-semibold rounded py-5 px-6 bg-Rich-Black hover:bg-Prussian-Blue ease-in     '> Check Availability</button></Link>
             </div>
         </div>
     )
