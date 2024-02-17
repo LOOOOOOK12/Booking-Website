0c3cb04f7de172f267fdc4723998d15d29c0da69
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-17 18:54:37 +0800

    update

--- Booking-Website/src/Components/Date.jsx
+++ Booking-Website/src/Components/Date.jsx
@@ -4,7 +4,7 @@ function Date(props) {
     return (
         <div className='flex flex-col gap-5'>
             <label className='text-3xl text-Rich-Black font-semibold'>{props.title}</label>
-            <input type="date"/>
+            <input type="date" className='w-full px-4 py-2'/>
         </div>
     )
 }
