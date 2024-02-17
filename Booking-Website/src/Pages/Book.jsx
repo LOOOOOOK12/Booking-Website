331b2ebb6398b424abad01efe49a8611d4854e64
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-17 19:17:29 +0800

    update

--- Booking-Website/src/Pages/Book.jsx
+++ Booking-Website/src/Pages/Book.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Blogsbg from '../assets/Blogs/City.jpg'
+import street from '../assets/Book/street.jpg'
 import Availability from '../Components/Availability'
 import BookSection1 from '../Sections/BookSection/BookSection1'
 import BookSEction2 from '../Sections/BookSection/BookSEction2'
@@ -11,7 +12,7 @@ function Book() {
         <>
             <NavBar/>
                 <div className="h-[90vh] relative flex items-center justify-center">
-                    <img src={Blogsbg} alt="" className='absolute brightness-50 h-full w-full object-cover -z-10' />             
+                    <img src={street} alt="japan streets" className='absolute brightness-50 h-[110vh] w-full object-cover -z-10' />             
                     <h1 className='text-Silver-Lake-Blue text-7xl font-semibold'>Booking</h1>
                 </div>
                 <BookSection1/>
