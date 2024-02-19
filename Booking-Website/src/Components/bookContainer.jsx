f95aae3b40d3d9f304f5cf8992be37004d1daa76
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-02-19 19:02:59 +0800

    update

--- Booking-Website/src/Components/bookContainer.jsx
+++ Booking-Website/src/Components/bookContainer.jsx
@@ -5,7 +5,7 @@ function bookContainer({image, alt, header, price, description, visitors, count}
     return (
         <div className='flex flex-col'>
             <img src={image} alt={alt} className='rounded-3xl mb-5'/>
-            <div className='flex flex-col items-start gap-3'>
+            <div className='flex flex-col items-start gap-4'>
                 <h1 className='text-4xl text-Rich-Black font-semibold'>{header}</h1>
                 <h2 className='text-2xl text-Rich-Black font-semibold'>{price}</h2>
                 <div className='flex gap-5'>
@@ -13,7 +13,7 @@ function bookContainer({image, alt, header, price, description, visitors, count}
                     <p className='text-xl text-Rich-Black'>{visitors}</p>
                 </div>
                 <button 
-                    className='text-Puti px-6 py-5 w-full bg-Rich-Black hover:bg-Prussian-Blue ease-in duration-100 font-semibold rounded-md'
+                    className='text-Puti text-2xl px-6 py-5 w-full bg-Rich-Black hover:bg-Prussian-Blue ease-in duration-100 font-semibold rounded-md'
                     onClick={count}
                     >
                         Book
