7d7b29c9ddbb0973aafba205f67142eae68a23d6
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-30 16:35:18 +0800

    changes in logos

--- Booking-Website/src/Sections/ContactsSections/ContactsSection2.jsx
+++ Booking-Website/src/Sections/ContactsSections/ContactsSection2.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Accounts from '../../assets/Contacts/Account.png'
-import ComputerSupport from '../../assets/Contacts/ComputerSupport.png'
-import Mechanical from '../../assets/Contacts/Mechanical.png'
+import computerSupport from '../../assets/Contacts/cs.png'
+import Mechanical from '../../assets/Contacts/ms.png'
+import Accounts from '../../assets/Contacts/cuss.png'
 
 function ContactsSection2() {
     return (
         <div className='py-20 px-16 flex flex-col justify-between items-center gap-10 md:flex-row md:items-start'>
-            <div className='flex flex-col items-center'>
-                <div className='flex justify-center items-center'>
-                    <img src={ComputerSupport} alt="Technical Support" />
+            <div className='flex flex-col gap-3 items-center'>
+                <div className='flex justify-center gap-3 items-center'>
+                    <img src={computerSupport} alt="Technical Support" />
                     <h1 className=' text-Rich-Black text-3xl font-semibold mb-5'>Technical Support</h1>
                 </div>
                 <div className='flex flex-col gap-3'>
@@ -18,8 +18,8 @@ function ContactsSection2() {
                     <p>Jayce Lighthammer</p>
                 </div>
             </div>
-            <div className='flex flex-col items-center'>
-                <div className='flex gap-10 items-center'>
+            <div className='flex flex-col gap-3 items-center'>
+                <div className='flex gap-3 justify-center items-center'>
                     <img src={Mechanical} alt="MEchanical Support" />
                     <h1 className=' text-Rich-Black text-3xl font-semibold mb-5'>Mechanical Support</h1>
                 </div>
@@ -30,10 +30,10 @@ function ContactsSection2() {
                     <p>Zac Tabudlo</p>
                 </div>
             </div>
-            <div className='flex flex-col items-center'>
-                <div className='flex gap-10 items-center'>
+            <div className='flex flex-col gap-3 items-center'>
+                <div className='flex gap-3 justify-center items-center'>
                     <img src={Accounts} alt="Support" />
-                    <h1 className=' text-Rich-Black text-3xl font-semibold mb-5'>Support</h1>
+                    <h1 className=' text-Rich-Black text-3xl font-semibold mb-5'>Customer Support</h1>
                 </div>
                 <div className='flex flex-col gap-3'>
                     <p>Zyra Mae Dizon</p>
