e1d11ce09b53314a51ac5f73c2abaf2aa146f265
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-03-30 16:18:28 +0800

    fooeter items centered

--- Booking-Website/src/Components/Footer.jsx
+++ Booking-Website/src/Components/Footer.jsx
@@ -6,15 +6,15 @@ import {Link} from 'react-router-dom'
 
 function Footer() {
     return (
-        <div className='p-6 flex flex-col justify-evenly items-center gap-10 bg-Prussian-Blue text-Puti '>
-            <div className='w-full h-full flex justify-evenly'>
-                <div className='justify-start flex flex-col gap-2'>
+        <div className='p-6 flex flex-col md:justify-evenly items-center gap-10 bg-Prussian-Blue text-Puti '>
+            <div className='w-full h-full flex-col justify-center items-center md:flex md:flex-row md:justify-evenly'>
+                <div className='items-center md:justify-start flex flex-col gap-2'>
                     <h2 className='mb-4 font-semibold text-2xl'>Contacts</h2>
                     <p>JohnDoe.com</p>
                     <p>Website@gmail.com</p>
                     <p>Employees</p>
                 </div>
-                <div className='justify-start flex flex-col gap-2'>
+                <div className='items-center m-5 md:justify-start flex flex-col gap-2'>
                     <h2 className='mb-4 font-semibold text-2xl'>Menu</h2>
                     <Link to="/Home" className='Links'><p>Home</p></Link>
                     <Link to="/About" className='Links'><p>About Us</p></Link>
@@ -22,7 +22,7 @@ function Footer() {
                     <Link to="/Services" className='Links'><p>Services</p></Link>
                     <Link to="/Contacts" className='Links'><p>Contacts</p></Link> 
                 </div>
-                <div className='justify-start flex flex-col gap-2'>
+                <div className='items-center m-5 md:justify-start flex flex-col gap-2'>
                     <h2 className='mb-4 font-semibold text-2xl'>Media</h2>
                     <div className='flex items-center gap-5'>
                         <img src={X} alt="" />
