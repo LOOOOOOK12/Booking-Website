00d67546e85222e6ca61e84a72a4d173ec0efba8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-03 17:47:06 +0800

    bg brightness adjusted

--- Booking-Website/src/Pages/Blogs.jsx
+++ Booking-Website/src/Pages/Blogs.jsx
@@ -12,7 +12,7 @@ function Blogs() {
         <>
             <NavBar/>
                 <div className="h-[90vh] relative flex items-center justify-center">
-                    <img src={Blogsbg} alt="" className='absolute brightness-50 h-full w-full object-cover -z-10' />             
+                    <img src={Blogsbg} alt="" className='absolute brightness-[.26] h-full w-full object-cover -z-10' />             
                     <h1 className='text-7xl font-semibold text-Puti'>Our <span className='text-Silver-Lake-Blue'>Blogs</span></h1>
                 </div>
                 <Availability/>
