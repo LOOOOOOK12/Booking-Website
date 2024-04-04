cbdab3f99f7f35460688d5f0d29907b39b323c5e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-04 22:02:15 +0800

    fixes

--- Booking-Website/src/Components/HotelsContainer.jsx
+++ Booking-Website/src/Components/HotelsContainer.jsx
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom'
 function HotelsContainer({image, alt, hotelName, cost, wifi, service, bed, person}){
 
     return (
-        <div className='flex flex-row bg-Grey-2 gap-6 rounded-md md:flex md:flex-col md:justify-center md:items-center md:text-center md:gap-6 md:pb-7 md:bg-Grey-2 '>           
+        <div className='flex flex-col items-center justify-center bg-Grey-2 gap-6 rounded-md md:flex md:flex-col md:justify-center md:items-center md:text-center md:gap-6 md:pb-7 md:bg-Grey-2 '>           
             <div className='w-full'>
                 <img src={image} alt={alt} className='h-full w-full rounded-md'/>
             </div>   
-            <div className='flex flex-col gap-5'>
+            <div className='flex flex-col items-center justify-center gap-5'>
                 <div className=''>
                     <h3 className='text-Rich-Black text-4xl font-semibold mb-6'>{hotelName}</h3>
                     <hr className='bg-Rich-Black h-0.5'/>
                 </div>
-                <div className='flex flex-col gap-5 text-Grey text-xl'>
+                <div className='flex flex-col items-center justify-center gap-5 text-Grey text-xl'>
                     <p>{cost}</p>
                     <p>{wifi}</p>
                     <p>{service}</p>
                     <p>{bed}</p>
                     <p>{person}</p>
                     <Link to="/Book">
-                        <button className='px-11 py-4 text-2xl font-semibold bg-Rich-Black hover:bg-Prussian-Blue ease-in duration-100 text-Puti rounded-md'>
+                        <button className='px-11 py-4 text-2xl mb-9 font-semibold bg-Rich-Black hover:bg-Prussian-Blue ease-in duration-100 text-Puti rounded-md'>
                             Check
                         </button>
                     </Link>
